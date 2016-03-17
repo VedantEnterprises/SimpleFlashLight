ca95ac4e77026e1920fdacd397cec9db4f1dccd3
joielechong <joielechong@users.noreply.github.com>
2016-03-17 23:42:17 +0700

    Add support for device without flash camera

--- app/src/main/java/com/rilixtech/simpleflashlight/FlashLightActivity.java
+++ app/src/main/java/com/rilixtech/simpleflashlight/FlashLightActivity.java
@@ -10,13 +10,21 @@ import android.text.SpannableString;
 import android.text.method.LinkMovementMethod;
 import android.text.util.Linkify;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
 public class FlashLightActivity extends Activity implements ToggleButton.OnClickListener {
-  private Camera cam = Camera.open();          // used to open
-  private Parameters params = cam.getParameters();  // the camera and flashlight
+  //private Camera cam = Camera.open();          // used to open
+  //private Parameters params = cam.getParameters();  // the camera and flashlight
+  private Camera cam;          // used to open
+  private Parameters params;  // the camera and flashlight
+  private boolean hasFlashCamera;
+  private Window mWindow;
+  private WindowManager.LayoutParams mLayoutParams;
+  private WindowManager.LayoutParams mOldLayoutParams;
 
   protected void onCreate(Bundle savedInstanceState) {
     //boolean flash = params.getFlashMode().equals(Parameters.FLASH_MODE_TORCH);
@@ -24,6 +32,9 @@ public class FlashLightActivity extends Activity implements ToggleButton.OnClick
     setContentView(R.layout.main_window);
     ToggleButton tb = (ToggleButton) findViewById(R.id.main_toggle_btn);
     tb.setOnClickListener(this);
+
+    // check flash camera, is there is one?
+    hasDeviceFlashCamera();
     tb.setChecked(false);
 
     Button aboutBtn = (Button) findViewById(R.id.main_about_btn);
@@ -32,20 +43,72 @@ public class FlashLightActivity extends Activity implements ToggleButton.OnClick
         AlertDialog(getString(R.string.app_name), getString(R.string.about_content), "OK");
       }
     });
+
+
+  }
+
+  /**
+   * Check if device have a flash camera
+   * @return
+   */
+  private void hasDeviceFlashCamera() {
+    try {
+      cam = Camera.open();
+      params = cam.getParameters();
+      // try set a flash mode to camera, if success then device support flash camera.
+      params.setFlashMode(Parameters.FLASH_MODE_OFF);
+      hasFlashCamera = true;
+    } catch (RuntimeException ex) {
+      mWindow = getWindow();
+      mLayoutParams = mWindow.getAttributes();
+      mOldLayoutParams = mLayoutParams;
+      hasFlashCamera = false;
+    }
+  }
+
+  private void turnDisplayOn() {
+    // Flashlights should be bright.
+    mLayoutParams.screenBrightness = WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_FULL;
+    // params.buttonBrightness was added in API level 8. The additional brightness is not worth the added code size.
+    // The screen will go to max brightness even without the following line, but the API doesn't guarantee it.
+    mWindow.setAttributes(mLayoutParams);
+    mWindow.addFlags(
+        // Use the power button to turn flashlight off and on, even if you have a lock screen.
+        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+            // A flashlight that turns itself off isn't a good flashlight.
+            | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+            // Turn the screen on if it isn't already on when launching (e.g., from ADB).
+            | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+
+  }
+
+  private void turnDisplayOff() {
+    mLayoutParams.screenBrightness = WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_NONE;
+    mWindow.setAttributes(mLayoutParams);
+    mWindow.clearFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+    mWindow.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    mWindow.clearFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
   }
 
   // turns the flashlight on:
-  private boolean turnOn() {
-    cam.startPreview();
-    params.setFlashMode(Parameters.FLASH_MODE_TORCH);
-    cam.setParameters(params);
-    return true;
+  private void turnOn() {
+    if(hasFlashCamera) {
+      cam.startPreview();
+      params.setFlashMode(Parameters.FLASH_MODE_TORCH);
+      cam.setParameters(params);
+    } else {
+      turnDisplayOn();
+    }
   }
 
   // turns the flashlight off:
   private void turnOff() {
-    params.setFlashMode(Parameters.FLASH_MODE_OFF);
-    cam.setParameters(params);
+    if(hasFlashCamera) {
+      params.setFlashMode(Parameters.FLASH_MODE_OFF);
+      cam.setParameters(params);
+    } else {
+      turnDisplayOff();
+    }
   }
 
   private void AlertDialog(String title, String msg, String btn) {
@@ -67,9 +130,11 @@ public class FlashLightActivity extends Activity implements ToggleButton.OnClick
   // when the app is closed:
   protected void onDestroy() {
     turnOff();
-    cam.stopPreview();
-    cam.setPreviewCallback(null);
-    cam.release();
+    if(hasFlashCamera) {
+      cam.stopPreview();
+      cam.setPreviewCallback(null);
+      cam.release();
+    }
     super.onDestroy();
   }
 
